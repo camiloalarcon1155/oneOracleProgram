3accbbadea2ab469dc7a71b6dc7b0ff506f760ac
Camilo Alarcón <camiloalarcon11155@gmail.com>
2024-07-01 19:44:04 -0500

    1ra carpeta de curso inicial de oneOracle

---
+++ 2034-logica-programacion-1-Aula1/ejercicios1Curso.js
@@ -0,0 +1,28 @@
+//alert('Hola wacho');
+
+alert("¡Bienvenida y bienvenido a nuestro sitio web!");
+let nombre = "Luna";
+let edad = 25;
+let numeroDeVentas = 50;
+let saldoDisponible = 1000;
+let mensajeDeError = "¡Error! Completa todos los campos";
+alert(mensajeDeError)
+let nombreUsuario = prompt('Ingrese su nombre: ')
+let edadUsuario = prompt('Ingrese su edad: ')
+
+if (edadUsuario >= 18) {
+    alert('¡Puedes obtener tu licencia de conducir!');
+}
+/* Desafíos
+
+Muestra una alerta con el mensaje "¡Bienvenida y bienvenido a nuestro sitio web!".
+Declara una variable llamada nombre y asígnale el valor "Luna".
+Crea una variable llamada edady asígnale el valor 25.
+Establece una variable numeroDeVentas y asígnale el valor 50.
+Establece una variable saldoDisponible y asígnale el valor 1000.
+Muestra una alerta con el texto "¡Error! Completa todos los campos".
+Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". Ahora muestra una alerta con el valor de la variable mensajeDeError .
+Utiliza un prompt para preguntar el nombre del usuario y almacénalo en la variable nombre .
+Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
+Si necesitas ayuda, las soluciones a las actividades estarán disponibles en la sección "Opinión del instructor". */
---
+++ notasOneOracle.js
@@ -0,0 +1,1 @@
+//escribir con camell algo. por que variables quedan con la joronbba de camello
